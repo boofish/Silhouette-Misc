d4e96592b240f25417c9f7e54a1e172b4ebdd62e
Jie Zhou <jzhou41@cs.rochester.edu>
2019-03-01 20:05:06 -0500

    Added two small test programs
    These two small test programs are to help the development of LLVM
    MachineFunction passes.

---
+++ programs/small-tests/README.md
@@ -0,0 +1,4 @@
+# Small handwritten test programs
+
+The qsort program was [randomly
+found](ftp://ftp.math.utah.edu/pub/tex/dviold/qsort.c) online.
---
+++ programs/small-tests/hello.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+#define BASE(i) &base[(i)*width]
+
+int foo(int num) {
+    int i = 0;
+    int total = 1;
+
+    for (i = 10; i > 0; i--) {
+        total += i;
+    }
+
+
+    return total + num;
+}
+
+
+void
+qsort(base, nel, width, compar)
+    char base[];	/* start of data in memory */
+    int nel;		/* number of elements to be sorted */
+    int width;		/* size (in bytes) of each element */
+    int (*compar)();	/* comparison function */
+{
+    int gap;
+    int i;
+    int j;
+
+    register int k;	/* inner exchange loop parameters */
+    register char* p;
+    register char* q;
+    register char  c;
+
+    for (gap = nel/2; gap > 0; gap /= 2)
+    {
+	for (i = gap; i < nel; i++)
+	{
+	    for (j = i-gap; j >= 0; j -= gap)
+	    {
+	        p = BASE(j);
+		q = BASE(j+gap);
+		if ((*compar)(p,q) <= 0)
+		    break;	/* exit j loop */
+		else
+		{
+		    for (k = 0; k < width; (++p, ++q, ++k))
+		    {
+			c = *q;
+			*q = *p;
+			*p = c;
+		    }
+		}
+	    }
+	}
+    }
+}
+
+int main() {
+    char buffer[6];
+    sprintf(buffer, "Hello!\n");
+}
