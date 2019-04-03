d54ad514c9a473db947812cd8c152f0ea56792f2
Jie Zhou <jzhou41@cs.rochester.edu>
2019-04-03 16:49:04 -0400

    Updated compile_run.sh
    Generate an asm file after compilation of each program.

--- scripts/compile_run.sh
+++ scripts/compile_run.sh
@@ -15,8 +15,10 @@ SCRIPTS_DIR=`pwd`
 BEEBS_PROJ=$SILHOUETTE/projs/beebs
 BEEBS_ELF=$BEEBS_PROJ/Debug/beebs.elf
 BEEBS_RUN_CFG="$BEEBS_PROJ/beebs Run.cfg"
-BEEBS_DIR=$SILHOUETTE/silhouette-misc/programs/beebs
-BEEBS_SRC=$BEEBS_DIR/beebs
+BEEBS_SRC=$SILHOUETTE/silhouette-misc/programs/beebs/beebs/src
+
+# GNU ARM toolchain
+OBJDUMP=/home/jie/local/gcc-arm-none-eabi-8-2018-q4-major/bin/arm-none-eabi-objdump
 
 SRC_BLACKLIST="crc32 ctl ctl-stack ctl-vector fdct"
 SRC_WHITELIST="aha-compress aha-mont64 bs bubblesort cnt compress cover crc    \
@@ -36,6 +38,11 @@ fac"
 # directory located in ~/projects/silhouette/. If not, make one.
 #
 function compile() {
+    if [ ! -d $BEEBS_SRC/$1 ]; then
+        echo "There is no $1 in beebs benchmark suite."
+        return 1
+    fi
+
     # Update source file in the beebs project source tree.
     ./init_proj.sh $1
 
@@ -46,19 +53,26 @@ function compile() {
         echo "Creating a $1 directory in silhouette/debug"
         mkdir -p $1
     fi
-    cd $SCRIPTS_DIR
+    DEBUG_DIR=$SILHOUETTE/debug/$1
+
+    # cleanup the debug directory for the program
+    rm -f $DEBUG_DIR/* 
 
     # compile
+    cd $SCRIPTS_DIR
     echo "Compiling $1 ......"
-    make build >& $SILHOUETTE/debug/$1/build.log
+    make build >& $DEBUG_DIR/build.log
     echo -e "Done compiling $1"
     
     # update the Run.cfg file
     sed -i 's/.*stlink.*/source \[find interface\/stlink-v2-1.cfg]/' $BEEBS_PROJ/beebs\ Run.cfg
 
-    # Copy the elf to the debug directory
+    # Copy the elf to the debug directory and change the name from beebs.efl
+    # to prog_name.elf. Also generate the asm code.
     echo -e "Copying $1.elf to debug/$1\n"
-    cp $BEEBS_PROJ/Debug/beebs.elf $SILHOUETTE/debug/$1
+    cp $BEEBS_PROJ/Debug/beebs.elf $DEBUG_DIR/$1.elf
+    cd $DEBUG_DIR
+    $OBJDUMP -d $1.elf > $1.s
 }
 
 
@@ -77,13 +91,11 @@ if [ ! $1 == "" ]; then
     if [ $1 == "run" ]; then
         run $1
     else 
-        compile $1
-        run $1
+        compile $1 && run $1
     fi
 else
     for prog in $TEST_FILES; do
         echo "Compile and Run $prog"
-        compile $prog
-        run $prog
+        compile $prog && run $prog
     done
 fi
