79fd5178944aed05e670460a0f37530fad1e09d0
Jie Zhou <jzhou41@cs.rochester.edu>
2019-04-03 13:29:14 -0400

    Updated the compile_run script.

--- scripts/compile_run.sh
+++ scripts/compile_run.sh
@@ -4,11 +4,17 @@
 # This script compiles and runs all the test programs in BEEBS, except those 
 # that failed to compile or run even with original Clang 4.0.
 #
+# Command line arguments:
+#   $1 - the name of the program to compile and run. or just run a program if 
+#        $1 == "run". This is optional.
+#
 
 SILHOUETTE=~/projects/silhouette
 SCRIPTS_DIR=`pwd`
 
 BEEBS_PROJ=$SILHOUETTE/projs/beebs
+BEEBS_ELF=$BEEBS_PROJ/Debug/beebs.elf
+BEEBS_RUN_CFG="$BEEBS_PROJ/beebs Run.cfg"
 BEEBS_DIR=$SILHOUETTE/silhouette-misc/programs/beebs
 BEEBS_SRC=$BEEBS_DIR/beebs
 
@@ -17,7 +23,10 @@ SRC_WHITELIST="aha-compress aha-mont64 bs bubblesort cnt compress cover crc    \
                ctl-string cubic dijkstra dtoa duff edn expint fac fasta        \
                fibcall fir frac huffbench"
 
-TEST_FILES="fasta
+TEST_FILES="cubic
+aha-compress
+aha-mont64
+bs
 fac"
 
 
@@ -43,6 +52,9 @@ function compile() {
     echo "Compiling $1 ......"
     make build >& $SILHOUETTE/debug/$1/build.log
     echo -e "Done compiling $1"
+    
+    # update the Run.cfg file
+    sed -i 's/.*stlink.*/source \[find interface\/stlink-v2-1.cfg]/' $BEEBS_PROJ/beebs\ Run.cfg
 
     # Copy the elf to the debug directory
     echo -e "Copying $1.elf to debug/$1\n"
@@ -51,9 +63,27 @@ function compile() {
 
 
 # 
+# Run a program.
+#
+function run() {
+    openocd -f "$BEEBS_RUN_CFG" -c "program $BEEBS_ELF verify reset exit"
+}
+
+
+# 
 # Entrance of the script.
 #
-for prog in $TEST_FILES; do
-    echo "Compile and Run $prog"
-    compile $prog
-done
+if [ ! $1 == "" ]; then
+    if [ $1 == "run" ]; then
+        run $1
+    else 
+        compile $1
+        run $1
+    fi
+else
+    for prog in $TEST_FILES; do
+        echo "Compile and Run $prog"
+        compile $prog
+        run $prog
+    done
+fi
